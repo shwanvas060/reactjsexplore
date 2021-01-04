0a4cd7812ebedab9dd30a257753e4616da8e4771
shwanvas060 <2019202060@annauniv.edu.in>
2021-01-04 14:04:06 +0530

    one like for one user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import {useState } from 'react';
 import './App.css';
 
 function App() {
+  //
+  let[counter,setCounter] = useState(0);
+  const[like, setLike] = useState(true);
+  
+  const incrementer = () => {
+    setLike((prev) => !prev);
+    if(like===true && counter <=0 ){
+      setCounter(++counter);
+    }
+    else{
+      setCounter((prev)=>prev-1)
+    }
+    console.log(counter, like);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={incrementer}>Like {counter}</button>
+ 
     </div>
   );
 }
+
 
 export default App;
